0ff55b4ed9b40edfba943492ea40b55cc8b69e14
atomtigerzoo <stein@atomtigerzoo.com>
2017-08-02 14:43:45 +0200

    Page and content

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 // JS Goes here - ES6 supported
+
+const services = document.querySelectorAll(".testService");
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".userUrl").onblur = urlFieldUpdated;
+}, false);
+
+function urlFieldUpdated(event) {
+  const userUrl = event.target.value;
+
+  // add url to services
+  for (const service of services) {
+    service.href = `${service.href}${userUrl}`;
+  }
+}
